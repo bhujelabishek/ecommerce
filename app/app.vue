194dcc3cb3cf5b535c7ec878846623e490b41e86
Abishek Bhujel <104608446+bhujelabishek@users.noreply.github.com>
2026-03-27 09:46:24 +0545

    loader added

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,21 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+
+const loading = ref(true);
+
+onMounted(() => {
+  setTimeout(() => {
+    loading.value = false
+  }, 1000) // simulate loading
+})
+</script>
+
+
 <template>
-  <div>
-    <NuxtPage />
-  </div>
+  <NuxtLayout/>
+  <Loader v-if="loading"/>
+  <NuxtPage v-else />
+    <NuxtLoadingIndicator/>
+    
+  <NuxtLayout/>
 </template>
